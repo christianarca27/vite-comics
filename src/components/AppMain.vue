562b89cb86d6aaffbc83720d8a9be9d296233898
christianarca27 <christian.arca.mail@gmail.com>
2023-03-27 16:06:53 +0200

    feat: costruito e stilizzato AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -2,20 +2,51 @@
 export default {
     data() {
         return {
-            content: "--> Content goes here <--",
+
         }
     },
 }
 </script>
 
 <template>
-    <div class="container">
-        {{ content }}
-    </div>
+    <main>
+        <div id="jumbo"></div>
+
+        <div id="card-container"></div>
+
+        <button>Load more</button>
+    </main>
 </template>
 
 <style lang="scss" scoped>
-.container {
-    padding: 90px 0;
+main {
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    gap: 3rem;
+
+    #jumbo {
+        width: 100%;
+        height: 400px;
+        background-image: url("/img/jumbotron.jpg");
+        background-size: cover;
+    }
+
+    #card-container {
+        max-width: 1200px;
+        width: 100%;
+        height: 600px;
+    }
+
+    button {
+        background-color: #0282F9;
+        margin-bottom: 1rem;
+        padding: .5rem 3rem;
+
+        color: white;
+        // font-size: 1.2rem;
+        font-weight: bold;
+        text-transform: uppercase;
+    }
 }
 </style>
