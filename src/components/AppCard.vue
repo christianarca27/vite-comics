8d9a927e73d7eda57ac025652093a6cf8a36e814
christianarca27 <christian.arca.mail@gmail.com>
2023-03-27 16:34:36 +0200

    feat: stilizzato card container + create e stilizzate AppCard e inserite al suo interno

---
+++ src/components/AppCard.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        img: String,
+        title: String,
+    }
+}
+</script>
+
+<template>
+    <div id="card">
+        <img :src="img" alt="comic thumb">
+        <p>{{ title }}</p>
+    </div>
+</template>
+
+<style lang="scss">
+#card {
+    width: calc(100% / 6 - 30px * 5 / 6);
+
+    img {
+        width: 100%;
+        aspect-ratio: 1;
+        margin-bottom: 1rem;
+
+        object-fit: cover;
+        object-position: top;
+    }
+
+    p {
+        text-transform: uppercase;
+    }
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,10 +1,14 @@
 <script>
+import AppCard from './AppCard.vue';
+import json from '../json/dc-comics.json'
+
 export default {
     data() {
         return {
-
-        }
+            comics: json,
+        };
     },
+    components: { AppCard },
 }
 </script>
 
@@ -12,7 +16,9 @@ export default {
     <main>
         <div id="jumbo"></div>
 
-        <div id="card-container"></div>
+        <div id="card-container">
+            <AppCard v-for="comic in comics" :img="comic.thumb" :title="comic.series"></AppCard>
+        </div>
 
         <button>Load more</button>
     </main>
@@ -35,7 +41,10 @@ main {
     #card-container {
         max-width: 1200px;
         width: 100%;
-        height: 600px;
+
+        display: flex;
+        flex-flow: row wrap;
+        gap: 30px;
     }
 
     button {
